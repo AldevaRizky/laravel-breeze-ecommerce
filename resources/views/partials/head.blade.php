855fb2ecfe14aae52283811df23d82a9b35b02a9
Aldeva Rizky <aldevarwpp@gmail.com>
2025-05-10 18:02:25 +0700

    feat(frontend): add additional icon libraries (Bootstrap Icons, Font Awesome, and Material Icons)

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -10,3 +10,7 @@
 <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded">
 <link id="pagestyle" href="{{ asset('assets/css/material-dashboard.css?v=3.2.0') }}" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
