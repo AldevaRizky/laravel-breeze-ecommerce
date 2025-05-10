8e8cfb1e49035f6975a305f59476d7817818b1c4
Aldeva Rizky <aldevarwpp@gmail.com>
2025-05-10 18:05:15 +0700

    feat(admin-dashboard): display total users, categories, and products

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Product;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('admin.dashboard');
+        $totalUsers = User::count();
+        $totalCategories = Category::count();
+        $totalProducts = Product::count();
+
+        return view('admin.dashboard', compact('totalUsers', 'totalCategories', 'totalProducts'));
     }
 }
+
