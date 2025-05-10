ba953654f41767b389732591c6d59d3120d9cf07
Aldeva Rizky <aldevarwpp@gmail.com>
2025-05-10 18:01:52 +0700

    feat(admin-menu): improve menu structure with route checks and logout handling

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -3,6 +3,7 @@ $menus = [
     [
         'name' => 'Dashboard',
         'url' => route('admin.dashboard'),
+        'route' => 'admin.dashboard',
         'icon' => 'dashboard',
         'roles' => ['admin'],
     ],
@@ -12,21 +13,17 @@ $menus = [
     [
         'name' => 'Products',
         'url' => route('admin.products.index'),
+        'route' => 'admin.products.index',
         'icon' => 'inventory_2',
         'roles' => ['admin'],
     ],
     [
         'name' => 'Categories',
         'url' => route('admin.categories.index'),
+        'route' => 'admin.categories.index',
         'icon' => 'category',
         'roles' => ['admin'],
     ],
-    // [
-    //     'name' => 'Users',
-    //     'url' => route('admin.users.index'),
-    //     'icon' => 'table',
-    //     'roles' => ['admin'],
-    // ],
     [
         'group' => 'Account pages',
     ],
@@ -35,14 +32,13 @@ $menus = [
         'url' => route('logout'),
         'icon' => 'logout',
         'roles' => ['admin', 'pt'],
+        'is_logout' => true,
     ],
 ];
 @endphp
 
-
 <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-radius-lg fixed-start ms-2 bg-white my-2" id="sidenav-main">
   <div class="sidenav-header">
-    <i class="fas fa-times p-3 cursor-pointer text-dark opacity-5 position-absolute end-0 top-0 d-none d-xl-none" id="iconSidenav"></i>
     <a class="navbar-brand px-4 py-3 m-0" href="#">
       <img src="/assets/img/logo-ct-dark.png" class="navbar-brand-img" width="26" height="26" alt="main_logo">
       <span class="ms-1 text-sm text-dark">{{ config('app.name') }}</span>
@@ -52,24 +48,34 @@ $menus = [
   <div class="collapse navbar-collapse w-auto" id="sidenav-collapse-main">
     <ul class="navbar-nav">
         @foreach ($menus as $menu)
-          @if (isset($menu['group']))
-            <li class="nav-item mt-3">
-              <h6 class="ps-4 ms-2 text-uppercase text-xs text-dark font-weight-bolder opacity-5">
-                {{ $menu['group'] }}
-              </h6>
-            </li>
-          @elseif (in_array(auth()->user()->role, $menu['roles']))
-            @php
-              $isActive = request()->url() === $menu['url'];
-            @endphp
-            <li class="nav-item">
-              <a class="nav-link {{ $isActive ? 'active bg-gradient-dark text-white' : 'text-dark' }}" href="{{ $menu['url'] }}">
-                <i class="material-symbols-rounded opacity-5">{{ $menu['icon'] }}</i>
-                <span class="nav-link-text ms-1">{{ $menu['name'] }}</span>
-              </a>
-            </li>
-          @endif
+            @if (isset($menu['group']))
+                <li class="nav-item mt-3">
+                    <h6 class="ps-4 ms-2 text-uppercase text-xs text-dark font-weight-bolder opacity-5">
+                        {{ $menu['group'] }}
+                    </h6>
+                </li>
+            @elseif (in_array(auth()->user()->role, $menu['roles']))
+                @php
+                    $isActive = isset($menu['route']) && request()->routeIs($menu['route']);
+                @endphp
+                <li class="nav-item">
+                    @if (!empty($menu['is_logout']))
+                        <form id="logout-form" action="{{ $menu['url'] }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                        <a href="#" class="nav-link text-dark" id="logout-btn">
+                            <i class="material-symbols-rounded opacity-5">{{ $menu['icon'] }}</i>
+                            <span class="nav-link-text ms-1">{{ $menu['name'] }}</span>
+                        </a>
+                    @else
+                        <a class="nav-link {{ $isActive ? 'active bg-gradient-dark text-white' : 'text-dark' }}" href="{{ $menu['url'] }}">
+                            <i class="material-symbols-rounded opacity-5">{{ $menu['icon'] }}</i>
+                            <span class="nav-link-text ms-1">{{ $menu['name'] }}</span>
+                        </a>
+                    @endif
+                </li>
+            @endif
         @endforeach
-      </ul>
+    </ul>
   </div>
 </aside>
